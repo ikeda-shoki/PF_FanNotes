d4472d1ada1be8e44b172a93299c46f34a064b2b
ikeda-shoki <ikeda963552@gmail.com>
2021-03-03 03:27:07 +0000

    [Add]post_image/new&create

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -12,4 +12,35 @@ class PostImagesController < ApplicationController
     redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
   end
   
+  def new
+    @post_image = PostImage.new
+  end
+  
+  def show
+  end
+  
+  def index
+    @post_images = PostImage.all
+  end
+  
+  def create
+    @post_image = PostImage.new(post_image_params)
+    @post_image.save
+    redirect_to post_images_path
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
+  private
+    def post_image_params
+      params.require(:post_image).permit(:title, :image, :image_introduction, :user_id).merge(user_id: current_user.id)
+    end
+  
 end
