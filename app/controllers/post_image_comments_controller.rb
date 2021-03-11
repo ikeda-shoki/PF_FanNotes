e39f5e2f6e417925ff2d8f33701a1cfbe4f3d722
ikeda-shoki <ikeda963552@gmail.com>
2021-03-11 01:45:11 +0000

    [Add]非同期処理/favorite&post_image_comment

--- app/controllers/post_image_comments_controller.rb
+++ app/controllers/post_image_comments_controller.rb
@@ -3,22 +3,18 @@ class PostImageCommentsController < ApplicationController
 
   def create
     @post_image = PostImage.find(params[:post_image_id])
+    @post_image_comments = @post_image.post_image_comments.order('created_at DESC')
     @post_image_comment = PostImageComment.new(post_image_comment_params)
     @post_image_comment.post_image_id = @post_image.id
-    if @post_image_comment.save
-      redirect_to post_image_path(@post_image)
-    else
-      @post_image_comments = @post_image.post_image_comments
-      render 'post_images/show'
+    unless @post_image_comment.save
+      render 'error'
     end
   end
 
   def destroy
-    if PostImageComment.find_by(id: params[:id], post_image_id: params[:post_image_id]).destroy
-      redirect_back(fallback_location: post_image_path(params[:post_image_id]))
-    else
-      render 'post_images/show'
-    end
+    @post_image = PostImage.find_by(id: params[:post_image_id])
+    @post_image_comments = @post_image.post_image_comments.order('created_at DESC')
+    PostImageComment.find_by(id: params[:id], post_image_id: params[:post_image_id]).destroy
   end
 
   private
