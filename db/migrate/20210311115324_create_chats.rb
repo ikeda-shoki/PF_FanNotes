c19537af22d44884017ba8570d6b659b9d4f8cde
ikeda-shoki <ikeda963552@gmail.com>
2021-03-14 09:38:11 +0000

    [Fix]db

--- db/migrate/20210311115324_create_chats.rb
+++ db/migrate/20210311115324_create_chats.rb
@@ -2,7 +2,7 @@ class CreateChats < ActiveRecord::Migration[5.2]
   def change
     create_table :chats do |t|
       t.references :user, foreign_key: true
-      t.references :room_id, foreign_key: true
+      t.references :room_, foreign_key: true
       t.text :message
 
       t.timestamps
--- db/migrate/20210311120015_rename_room_id_id_column_to_chats.rb
+++
@@ -1,5 +0,0 @@
-class RenameRoomIdIdColumnToChats < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :chats, :room_id_id, :room_id
-  end
-end
