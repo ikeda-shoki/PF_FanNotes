d5b6f22ea9cd7fdb49920709fc345cbf58c8102f
ikeda-shoki <ikeda963552@gmail.com>
2021-03-18 17:36:26 +0000

    [Fix]デザイン/controller/search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,11 +2,20 @@ class SearchController < ApplicationController
 
   def search
    @user_or_image = params[:model]
-    if @user_or_image === "1"
-      @search_users = User.where(['account_name LIKE ?', "%#{params[:search]}%"])
-    elsif @user_or_image === "2"
-      @search_post_images = PostImage.where(['title LIKE ? OR image_introduction LIKE ?', "%#{params[:search]}%", "%#{params[:search]}%"])
-    end
+   @search_word = params[:search]
+   if @search_word.empty?
+     if @user_or_image === "1"
+       @search_users = []
+     elsif @user_or_image === "2"
+       @search_post_images = []
+     end
+   else
+     if @user_or_image === "1"
+       @search_users = Kaminari.paginate_array(User.where(['account_name LIKE ?', "%#{params[:search]}%"])).page(params[:page]).per(5)
+     elsif @user_or_image === "2"
+       @search_post_images = Kaminari.paginate_array(PostImage.where(['title LIKE ? OR image_introduction LIKE ?', "%#{params[:search]}%", "%#{params[:search]}%"])).page(params[:page]).per(8)
+     end
+   end
   end
   
   # post_image 並び替え
