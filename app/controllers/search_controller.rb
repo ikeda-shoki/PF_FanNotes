b261820ff9d24dea69a309274d6f55fae3ac727f
ikeda-shoki <ikeda963552@gmail.com>
2021-04-03 18:20:13 +0000

    [Add]post_image_sort/genre分け

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -20,16 +20,40 @@ class SearchController < ApplicationController
 
   # post_image 並び替え
   def post_image_search
-    @selection = params[:keyword]
+    session[:keyword] = params[:keyword]
+    @selection = session[:keyword]
     @sort_post_images = Kaminari.paginate_array(PostImage.sort(@selection)).page(params[:page]).per(8)
-    render 'post_images/index'
+    respond_to do |format|
+      format.html { render 'post_images/index' }
+      format.js
+    end
   end
 
   # user 並び替え
   def user_search
     @selection = params[:keyword]
-    @sort_users = Kaminari.paginate_array(User.sort(@selection)).page(params[:page]).per(5)
+    @sort_users = Kaminari.paginate_array(User.sort(@selection)).page(params[:page]).per(4)
     render 'users/index'
   end
 
+  def post_image_genre_search
+    @post_images = PostImage.all.page(params[:page]).per(8)
+    @selection = session[:keyword]
+    sort_images = PostImage.sort(@selection)
+    genre = params[:genre]
+    case genre
+    when "0" then
+      @sort_post_images = Kaminari.paginate_array(sort_images).page(params[:page]).per(8)
+    when "1" then
+      @sort_post_images = Kaminari.paginate_array(sort_images.select { |n| n.post_image_genre === "イラスト"}).page(params[:page]).per(8)
+    when "2" then
+      @sort_post_images = Kaminari.paginate_array(sort_images.select { |n| n.post_image_genre === "写真"}).page(params[:page]).per(8)
+    else
+      @sort_post_images = Kaminari.paginate_array(sort_images.select { |n| n.post_image_genre === "ロゴ"}).page(params[:page]).per(8)
+    end
+    respond_to do |format|
+      format.html { render 'post_images/index' }
+      format.js
+    end
+  end
 end
