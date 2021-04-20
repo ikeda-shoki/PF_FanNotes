74ff0626ccb7b2ae7e045136e0368fac7d9c0481
ikeda-shoki <ikeda963552@gmail.com>
2021-04-20 11:59:34 +0000

    [Fix]

--- config/routes.rb
+++ config/routes.rb
@@ -51,5 +51,4 @@ Rails.application.routes.draw do
   post 'follow/:id', to: 'relationships#follow', as: 'follow'
   delete 'follow/:id', to: 'relationships#unfollow', as: 'unfollow'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
 end
